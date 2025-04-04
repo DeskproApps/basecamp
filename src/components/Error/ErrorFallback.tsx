100c6798784487ca1106cd8dca05c7c47c61dbcb
OJ Abba <phavorabba@gmail.com>
2025-04-04 11:35:32 +0100

    [Bug] Improve error message display logic in `ErrorFallback`

--- src/components/Error/ErrorFallback.tsx
+++ src/components/Error/ErrorFallback.tsx
@@ -1,10 +1,9 @@
-import get from "lodash/get";
-import { Stack } from "@deskpro/deskpro-ui";
 import { BasecampError } from "../../services/basecamp";
-import { DEFAULT_ERROR } from "../../constants";
 import { Container, ErrorBlock } from "../common";
-import type { FC } from "react";
+import { DEFAULT_ERROR } from "../../constants";
+import { Stack } from "@deskpro/deskpro-ui";
 import type { FallbackProps } from "react-error-boundary";
+import type { FC } from "react";
 
 type Props = Omit<FallbackProps, "error"> & {
   error: Error,
@@ -18,14 +17,18 @@ const ErrorFallback: FC<Props> = ({ error }) => {
   console.error(error);
 
   if (error instanceof BasecampError) {
-    message = get(error, ["data", "error"], DEFAULT_ERROR);
+    message = error.data?.error ?? DEFAULT_ERROR
+  } else if (error instanceof Error) {
+    message = error.message.trim() !== "" ? error.message : DEFAULT_ERROR
+  } else {
+    message = DEFAULT_ERROR
   }
 
   return (
     <Container>
       <ErrorBlock
         text={(
-          <Stack gap={6} vertical style={{padding: "8px"}}>
+          <Stack gap={6} vertical style={{ padding: "8px" }}>
             {message}
             {button}
           </Stack>
