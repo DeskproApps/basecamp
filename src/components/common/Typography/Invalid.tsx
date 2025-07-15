2c82233316058dea689e95590cd59e83ca7ff95f
Paul <1740195+HappyPaul55@users.noreply.github.com>
2025-07-15 17:51:16 +0100

    [Chore] Upgrade deps 2025-07 (#67)

--- src/components/common/Typography/Invalid.tsx
+++ src/components/common/Typography/Invalid.tsx
@@ -2,12 +2,13 @@ import styled from "styled-components";
 import { TSpan } from "@deskpro/deskpro-ui";
 import type { TProps } from "@deskpro/deskpro-ui";
 import type { FC, PropsWithChildren } from "react";
+import type { DeskproAppTheme } from "@deskpro/app-sdk";
 
 type Props = PropsWithChildren<Omit<TProps, "type">> & {
   type?: TProps["type"],
 };
 
-const InvalidStyled = styled(TSpan)`
+const InvalidStyled = styled(TSpan)<DeskproAppTheme>`
   color: ${({ theme }) => theme.colors.red100};
 `;
 
